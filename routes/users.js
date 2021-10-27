500d3a2f8883fb57f40ad91f94ede9c894f8a959
Vennela <S544925@nwmissouri.edu>
2021-10-27 13:26:02 -0500

    added variable to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+let variable = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) { variable++;
+  res.send('User accesses are: '`User accesses are: ${variable}` );
 });
 
 module.exports = router;
